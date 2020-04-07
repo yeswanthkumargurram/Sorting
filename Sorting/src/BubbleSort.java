c3efcf6eb965514cca7ed82edacec252a18708c1
yeswanthkumargurram <yaswanthkumar189@gmail.com>
2020-04-07 12:54:48 +0530

    First commit

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,32 @@
+public class BubbleSort {
+
+	void bubbleSort(int[] arr) {
+		int n=arr.length;
+		for(int i=0;i<n-1;i++) {
+			for(int j=0;j<n-1-i;j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+	}
+	
+	void printArray(int[] arr) {
+		int n= arr.length;
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		BubbleSort bs=new BubbleSort();
+		int[] arr= {64, 34, 25, 12, 22, 11, 90};
+        bs.bubbleSort(arr);
+        System.out.println("Sorted Array");
+        bs.printArray(arr);
+        
+	}
+
+}
